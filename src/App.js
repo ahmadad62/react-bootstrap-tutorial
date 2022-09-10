eac9d16df217848d526a173933ab0cf3e09c7eed
ahmadad62 <a.naghian@gmail.com>
2022-09-10 10:41:04 +0200

    bootstrap was runned

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import { Alert } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Alert variant="primary">This is an Alert</Alert>
+        <Button>Test button</Button>
       </header>
     </div>
   );
