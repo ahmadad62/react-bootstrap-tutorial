4c9877b4c1fac420c4b0c537bd00b2bc6d777346
ahmadad62 <a.naghian@gmail.com>
2022-09-11 14:10:31 +0200

    add netlify contct form-and corect some issues

--- src/contact.js
+++ src/contact.js
@@ -11,22 +11,50 @@ function Contact() {
   return (
     <div className="Contact">
       <form
-        style={formStyles}
         name="contact v2"
-        method="POST"
+        method="post"
         data-netlify="true"
+        onSubmit="submit"
+        action="/success/"
+        data-netlify-honeypot="bot-field"
       >
-        <input type="hidden" name="forma-name" value="contact" />
-        <input required type="text" name="name" placeholder="Your Name" />
-        <input required type="email" name="email" placeholder="Your Email" />
-        <textarea
-          required
-          name="message"
-          placeholder="Please enter your Message"
-          col="30"
-          rows="10"
-        ></textarea>
-        <button type="submit">Send Message</button>
+        <input type="hidden" name="form-name" value="contact v2" />
+
+        <div hidden>
+          <input name="bot-field" />
+        </div>
+
+        <div>
+          <label>
+            First name
+            <br />
+            <input type="text" name="first-name" />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Last name
+            <br />
+            <input type="text" name="last-name" />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input id="email" type="email" name="email" />
+        </div>
+
+        <div>
+          <label>
+            Any Comments?
+            <br />
+            <textarea name="comments"></textarea>
+          </label>
+        </div>
+
+        <button type="submit">Submit The Results</button>
       </form>
     </div>
   );
